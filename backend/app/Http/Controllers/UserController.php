78a72a2697e8f3685513e9f5ee1879fd639d63b4
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-08-15 19:18:40 +0200

    add jwt

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+     
+
+    public function modify(Request $request){
+
+        $validated = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+        ]);
+        try{
+
+        }catch(Exception $e){
+
+        }
+    }
+
+    public function delete(Request $request){
+        try{
+
+        }catch(Exception $e){
+            
+        }
+    }
 }
