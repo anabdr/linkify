3f44172b859d01fe7c5c0de3de0c775315eb54e5
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-08-15 19:19:34 +0200

    add function add in LinkController

--- backend/app/Http/Controllers/LinkController.php
+++ backend/app/Http/Controllers/LinkController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Link;
+use Illuminate\Support\Str;
+use Validator;
 
 class LinkController extends Controller
 {
-    //
+    public function add(Request $request){
+
+        $validator = Validator::make(request()->all(), [
+            'url' => 'required',
+        ]);
+  
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        try{
+            $link = new Link();
+            $link->url = $request->url;
+            $link->code = Str::random(6);
+            $link->user_id = 1;
+            $link->save();
+
+            return response()->json($link->code, 200);
+            
+        }catch(Exception $e){
+            file_put_contents('errorAddLink', $e->getMessage());
+        }
+
+              
+
+    }
 }
