8a294a8393500a4047df151f23eb7c9744dae281
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-08-15 12:32:11 +0200

    add migrations

--- backend/database/migrations/2024_08_15_100906_create_links_table.php
+++ backend/database/migrations/2024_08_15_100906_create_links_table.php
@@ -11,16 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url')->nullable();
+            $table->string('code')->nullable();
+            $table->integer('userID')->nullable();
             $table->timestamps();
         });
-
     }
 
     /**
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('links');
     }
 };
