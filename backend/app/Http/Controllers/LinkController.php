e729343019b90871652860f49b634cdd372de7bb
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-08-17 17:52:07 +0200

    add function add and all to LinkController

--- backend/app/Http/Controllers/LinkController.php
+++ backend/app/Http/Controllers/LinkController.php
@@ -19,20 +19,70 @@ class LinkController extends Controller
             return response()->json($validator->errors()->toJson(), 400);
         }
 
+        $user = auth()->user();
+
         try{
-            $link = new Link();
-            $link->url = $request->url;
-            $link->code = Str::random(6);
-            $link->user_id = 1;
-            $link->save();
+
+            $exist = Link::where('url',"http://" . $request->url)->first();
+
+            if($exist){                
+                return response()->json($exist->code,200);
+            }else{
+                $link = new Link();
+                $link->url = "http://" . $request->url;
+                $link->code = Str::random(6);
+                $link->user_id = $user->id;;
+                $link->save();
+            }
+            
 
             return response()->json($link->code, 200);
             
         }catch(Exception $e){
-            file_put_contents('errorAddLink', $e->getMessage());
-        }
+            file_put_contents('errorAddLink.txt', $e->getMessage() . PHP_EOL, FILE_APPEND);
+        }              
 
-              
+    }
+
+    public function all(){
+        try{
+            $user = auth()->user();
+            
+            $links = Link::where('user_id',$user->id)->get();
+
+            return response()->json($links,200);
+
+        }catch(Exception $e){
+
+            file_put_contents('errorAllLink.txt', $e->getMessage() . PHP_EOL, FILE_APPEND);
+
+        }
+    }
+
+    public function redirectLink($link){
+
+        try{
+
+            $link = Link::where('code',$link)->first();            
+
+            if($link){
+
+                $link->count++;
+                $link->save();
+
+                return redirect()->away($link->url);
+
+            } else {
+
+                return response()->json('No se ha encontrado el link',500);
+            }
+
+
+        }catch (Exception $e) {
+
+            file_put_contents('errorRedirect.txt', $e->getMessage() . PHP_EOL, FILE_APPEND);
+
+        }
 
     }
 }
