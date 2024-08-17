aca8ff6d6998b4fdda8c7cae2cd83b97921b656e
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-08-17 17:55:43 +0200

    modificated app and main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,62 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+   
+    <HeaderNav v-if="token"/>
+
+    <div class="container">
+      <router-view />
+    </div>
+    
+  </div>
 </template>
 
+<script setup>
+import { computed } from 'vue'
+import HeaderNav from '@/components/HeaderNav'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const token = computed(() => store.state.token)
+</script>
+
 <style lang="scss">
+html, body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+  .container {
+    margin-top: 60px;
+    max-width: 900px;
+    margin: 0 auto;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    form {
+    display: flex;
+    flex-flow: column;
+    margin: 40px 0px;
+    
+      input {
+        padding: 15px 20px;
+        font-size: 1rem;
+        border: 1px solid #cecece;
+        border-radius: 4px;
+        margin-bottom: 10px;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &[type=submit] {
+          cursor: pointer;
+          background-color: #beebbe;
+          color: #2c782c;
+          font-weight: 700;
+        }
+      }
     }
   }
+  
 }
 </style>
