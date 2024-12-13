1f8f2c528106371f5d33b2cd043b72e01a244dae
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-12-13 13:05:46 +0100

    added bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,16 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueApexCharts from "vue3-apexcharts"
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
 
 createApp(App)
 .use(store)
